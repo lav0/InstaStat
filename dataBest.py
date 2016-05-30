7230424cd9f9802b0f4ee219a61088ac6ef6ce29
lav0 <andreigenuine@yahoo.com>
2016-05-30 18:23:13 +0200

    Some work on Best

--- dataBest.py
+++ dataBest.py
@@ -3,35 +3,75 @@ from mediaHolder import user_media
 from usersProvider import get_name_to_id_dict_from_db
 
 
-def get_media_sorted_by(user_name, principle):
-    user_dict = get_name_to_id_dict_from_db()
+class SortedMediaProvider:
+    def __init__(self, user_id=None, user_name=None):
+        self.user_id = None
+        if user_id is not None:
+            self.user_id = user_id
+        elif user_name is not None:
+            user_dict = get_name_to_id_dict_from_db()
+            if user_name in user_dict.keys():
+                self.user_id = user_dict[user_name]
 
-    user_id = user_dict[user_name]
-    all_media = user_media(user_id)
-    media_holders_list = list()
+        self.principle_to_media_holder_dict = dict()
 
-    for media in all_media:
-        media_holders_list.append(MediaHolder(media))
+    def is_defined(self):
+        return self.user_id is not None
 
-    sorted_media = sorted(media_holders_list, key=lambda m: m.like_count(), reverse=True)
+    def __make_sorted_media_by(self, principle):
+        if not self.is_defined():
+            print "Sorted media not defined."
+            return False
 
-    total_media_num = len(media_holders_list)
-    if total_media_num % 2 == 1:
-        mean = sorted_media[total_media_num / 2].like_count()
-    else:
-        left_mean = sorted_media[total_media_num / 2].like_count()
-        right_mean = sorted_media[total_media_num / 2 - 1].like_count()
-        mean = 0.5 * (left_mean + right_mean)
+        all_media = user_media(self.user_id)
+        if all_media is None:
+            print "User media not collected"
+            return False
 
-    print 'mean   : ', mean
-    print 'average: ', sum([m.like_count() for m in media_holders_list]) / float(len(media_holders_list))
+        media_holders_list = list()
 
-    return sorted_media
+        for media in all_media:
+            media_holders_list.append(MediaHolder(media))
+
+        sorted_media = sorted(media_holders_list, key=lambda m: m.like_count(), reverse=True)
+
+        self.principle_to_media_holder_dict[principle] = sorted_media
+
+        return True
+
+    def get_media_sorted_by(self, principle):
+        if principle not in self.principle_to_media_holder_dict.keys():
+            fine = self.__make_sorted_media_by(principle)
+            if not fine:
+                return list()
+
+        return self.principle_to_media_holder_dict[principle]
+
+    def get_media_mean_value_for(self, principle):
+        sorted_media = self.get_media_sorted_by(principle)
+
+        total_media_num = len(sorted_media)
+        if total_media_num % 2 == 1:
+            mean = sorted_media[total_media_num / 2].like_count()
+        else:
+            left_mean = sorted_media[total_media_num / 2].like_count()
+            right_mean = sorted_media[total_media_num / 2 - 1].like_count()
+            mean = 0.5 * (left_mean + right_mean)
+
+        return mean
+
+    def get_media_average_value_fro(self, principle):
+        sorted_media = self.get_media_sorted_by(principle)
+        return sum([m.like_count() for m in sorted_media]) / float(len(sorted_media))
 
 
-sorted_by_like = get_media_sorted_by('lav_q', 'like_count')
+provider = SortedMediaProvider(user_name='tlmnva0')
+sorted_by_like = provider.get_media_sorted_by('like_count')
 for p in sorted_by_like:
     print p.link(), p.like_count()
+
+print "Mean val:", provider.get_media_mean_value_for('like_count')
+print "Average:", provider.get_media_average_value_fro('like_count')
 
 
 
